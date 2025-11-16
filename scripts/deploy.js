72a1442860abe189fcfefd82c46a0cc57a1b3eb2
peykan <kingzalan666@gmail.com>
2025-11-16 01:56:03 +0100

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,25 @@
 
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying from:", deployer.address);
+
+  const Token = await hre.ethers.getContractFactory("MyToken");
+
+  // params: name, symbol, initialSupply (in wei)
+  const name = "MyToken";
+  const symbol = "MTK";
+  const initialSupply = hre.ethers.utils.parseUnits("1000000", 18); // 1,000,000 tokens
+
+  const token = await Token.deploy(name, symbol, initialSupply);
+  await token.deployed();
+
+  console.log("Token deployed to:", token.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+npx hardhat run --network baseSepolia scripts/deploy.js
